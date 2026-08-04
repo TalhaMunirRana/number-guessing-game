22a5bbdc9fc80422dba8a4134b492b2a1bd070ff
Talha Munir Rana <talhamunirrana@gmail.com>
2026-08-04 19:18:52 +0500

    Added invalid input checking and refactored the code

--- main.py
+++ main.py
@@ -5,17 +5,20 @@ print("--- Number Guessing Game ---")
 # Start the game loop
 while True:
     # Choose difficulty level
-    difficulty = input("Choose difficulty: \n1. Easy (1 -10)\n2. Medium (1 -50)\n3. Hard (1 -100)\n")
+    difficulty = input("\nChoose difficulty: \n1. Easy (1 -10)\n2. Medium (1 -50)\n3. Hard (1 -100)\n")
     if difficulty == '1':
         upper_limit = 10
     elif difficulty == '2':
         upper_limit = 50
     elif difficulty == '3':
         upper_limit = 100
+    else:
+        print("Invalid Input!")
+        continue
 
     # Generate the random number
-    number = random.randint(1, upper_limit)
-    print("I picked a number between 1 and " + str(upper_limit) + ".")
+    secret_number = random.randint(1, upper_limit)
+    print(f"I picked a number between 1 and {upper_limit}.")
 
     # Attempt counter
     attempts = 0
@@ -24,13 +27,13 @@ while True:
         guess = int(input("Your Guess: "))
         attempts += 1 # Increment 1 in attempts
 
-        if guess > number:
+        if guess > secret_number:
             print("Too High!")
-        elif guess < number:
+        elif guess < secret_number:
             print("Too Low!")
         else:
-            print("Congratulations! You guessed the number " + str(number) + ".")
-            print("You won in " + str(attempts) + " attempts.")
+            print(f"Congratulations! You guessed the number {secret_number}.")
+            print(f"You won in {attempts} attempts.")
             break
 
     again = input("Play again? (y/n) ")
