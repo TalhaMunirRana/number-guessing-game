b602e5ae02051dcc3c46b74e6f724a0b78d2d03c
Talha Munir Rana <talhamunirrana@gmail.com>
2026-08-04 18:39:46 +0500

    Added attempt counter

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ print("--- Number Guessing Game ---")
 
 # Generate a Random number
 number = random.randint(1, 10)
-
 print("I picked a number between 1 and 10")
+
+# Attempt counter
+attempts = 0
 
 while True:
     guess = int(input("Your Guess: "))
 
     if guess > number:
         print("Too High!")
+        attempts += 1 # Increment 1 in attempts
     elif guess < number:
         print("Too Low!")
+        attempts += 1 # Increment 1 in attempts
     else:
         print("Correct")
+        attempts += 1 # Increment 1 in attempts
+        print("Congratulations! You won in " + str(attempts) + " attempts.")
         break
