598b1b04155ce7d7bfa3ac9ebac3cd21d1571c74
Talha Munir Rana <talhamunirrana@gmail.com>
2026-08-04 19:10:25 +0500

    Added play again feature and refactored the code

--- main.py
+++ main.py
@@ -2,32 +2,40 @@ import random
 
 print("--- Number Guessing Game ---")
 
-# Choose difficulty level
-difficulty = input("Choose difficulty: \n1. Easy (1 -10)\n2. Medium (1 -50)\n3. Hard (1 -100)\n")
-if difficulty == '1':
-    upper_limit = 10
-elif difficulty == '2':
-    upper_limit = 50
-elif difficulty == '3':
-    upper_limit = 100
-
-# Generate the random number
-number = random.randint(1, upper_limit)
-print("I picked a number between 1 and " + str(upper_limit) + ".")
-
-# Attempt counter
-attempts = 0
-
+# Start the game loop
 while True:
-    guess = int(input("Your Guess: "))
-    attempts += 1 # Increment 1 in attempts
+    # Choose difficulty level
+    difficulty = input("Choose difficulty: \n1. Easy (1 -10)\n2. Medium (1 -50)\n3. Hard (1 -100)\n")
+    if difficulty == '1':
+        upper_limit = 10
+    elif difficulty == '2':
+        upper_limit = 50
+    elif difficulty == '3':
+        upper_limit = 100
 
-    if guess > number:
-        print("Too High!")
-        
-    elif guess < number:
-        print("Too Low!")
+    # Generate the random number
+    number = random.randint(1, upper_limit)
+    print("I picked a number between 1 and " + str(upper_limit) + ".")
+
+    # Attempt counter
+    attempts = 0
+
+    while True:
+        guess = int(input("Your Guess: "))
+        attempts += 1 # Increment 1 in attempts
+
+        if guess > number:
+            print("Too High!")
+        elif guess < number:
+            print("Too Low!")
+        else:
+            print("Congratulations! You guessed the number " + str(number) + ".")
+            print("You won in " + str(attempts) + " attempts.")
+            break
+
+    again = input("Play again? (y/n) ")
+
+    if again.lower() == 'y':
+        continue
     else:
-        print("Congratulations! You guessed the number " + str(number) + ".")
-        print("You won in " + str(attempts) + " attempts.")
         break
