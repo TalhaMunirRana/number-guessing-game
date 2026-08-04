655954fa502e7764fe53ef7afe8572290ce96c70
Talha Munir Rana <talhamunirrana@gmail.com>
2026-08-04 18:36:36 +0500

    Basic guessing game

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import random
 
 print("--- Number Guessing Game ---")
 
+# Generate a Random number
 number = random.randint(1, 10)
-
 
 print("I picked a number between 1 and 10")
 
@@ -16,9 +16,4 @@ while True:
         print("Too Low!")
     else:
         print("Correct")
-        play_again = input("Do you want to play again? (Yes/No) ")
-
-        if play_again.lower() == 'yes':
-            continue
-        elif play_again.lower() == 'no':
-            break
+        break
